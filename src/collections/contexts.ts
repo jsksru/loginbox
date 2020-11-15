2a7ec42af9b36f63036ac4d1541286e83b2f851b
jsksru <mail@jsks.ru>
2020-11-15 14:43:31 +0500

    theme context

--- src/collections/contexts.ts
+++ src/collections/contexts.ts
@@ -1,7 +1,8 @@
 import React from 'react';
 import EnglishLanguage from './lang/english';
+import * as theme from '../collections/themes/light';
 
-export const ThemeContext = React.createContext({});
+export const ThemeContext = React.createContext(theme);
 export const LangContext = React.createContext(EnglishLanguage);
 export const StateContext = React.createContext({
   currentLang: 0,
