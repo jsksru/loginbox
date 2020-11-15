c4a92fc5b3037ca434ba598e85cc89fc8f15b8a5
jsksru <mail@jsks.ru>
2020-11-15 14:43:56 +0500

    styling

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,45 +1,60 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { LangContext } from '../../collections/contexts';
-
-const LoginBoxWrapper = styled.div`
-  flex-grow: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const LoginBox = styled.div`
-  padding: 2rem;
-  border: 1px solid #ddd;
-  border-radius: 0.5rem;
-  background-color: #fff;
-  box-shadow: 0 1px 1px 0px #ddd;
-`;
+import { LangContext, ThemeContext } from '../../collections/contexts';
 
 const LoginForm = () => {
   const lang = useContext(LangContext);
+  const theme = useContext(ThemeContext);
+
+  const LoginBoxWrapper = styled.div`
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const LoginBox = styled.div`
+    ${theme.borderBox}
+  `;
+
+  const Title = styled.h1`
+    ${theme.title}
+    margin-bottom: 2rem;
+  `;
+
+  const Input = styled.input`
+    ${theme.inputTextField}
+  `;
+
+  const Button = styled.button`
+    ${theme.primaryButton}
+  `;
+
+  const LineDiv = styled.div`
+    margin-bottom: 1rem;
+  `;
+
   return (
     <LoginBoxWrapper>
       <LoginBox>
-        <div>{lang.formTitle}</div>
-        <div>
-          <input type="text" placeholder={lang.loginCaption}/>
-        </div>
-        <div>
-          <input type="text" placeholder={lang.passwordCaption}/>
-        </div>
-        <div>
+        <Title>{lang.formTitle}</Title>
+        <LineDiv>
+          <Input type="text" placeholder={lang.loginCaption}/>
+        </LineDiv>
+        <LineDiv>
+          <Input type="text" placeholder={lang.passwordCaption}/>
+        </LineDiv>
+        <LineDiv>
           <label>
             <input type="checkbox"/>
             <span>{lang.saveMeText}</span>
           </label>
-        </div>
-        <div>
+        </LineDiv>
+        <LineDiv>
           <a href="#">{lang.forgetPasswordText}</a>
-        </div>
+        </LineDiv>
         <div>
-          <button>{lang.enterButtonText}</button>
+          <Button>{lang.enterButtonText}</Button>
         </div>
       </LoginBox>
     </LoginBoxWrapper>
